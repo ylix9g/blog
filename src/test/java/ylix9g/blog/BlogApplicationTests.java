7e12a73f995f07c1174c1d49158841da281534e8
Igor Polichshuk <ylix9g@gmail.com>
2021-07-09 15:05:00 +0600

    Initialization

---
+++ src/test/java/ylix9g/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ylix9g.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+  @Test
+  void contextLoads() {
+    //
+  }
+}
